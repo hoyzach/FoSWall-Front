b742f8ff2e92931bff746f168b932f57e71e39f0
Zach Hoy <hoyzach@gmail.com>
2023-08-07 21:42:50 -0400

    contract update, fees accrued button, scroll buton

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,10 @@
 import './globals.css';
-
 import { Montserrat } from "@next/font/google"
 import { Providers } from './providers';
-
 import Footer from './footer';
 import Header from './header';
 import Subheader from './subheader';
+import ScrollToTopButton from '../utils/scrollToTopButton';
 
 const montserrat = Montserrat({
   weight: ["400", "700"],
@@ -29,8 +28,9 @@ export default function RootLayout({ children }) {
           <Header />
           <Subheader/>
           <main>
-            <div className="bg-primary bg-[url('/pattern3.png')] bg-cover bg-fixed w-full min-h-screen flex flex-1 flex-col items-center justify-center">
+            <div className="bg-primary bg-[url('/pattern3.png')] bg-cover bg-fixed w-full min-h-screen flex flex-1 flex-col items-center justify-center py-10">
               {children}
+              <ScrollToTopButton />
             </div>
           </main>
           <Footer />
