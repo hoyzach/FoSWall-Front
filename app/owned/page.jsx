c91fb45463df5c3dddbcd15507bdf09683dee3dc
Zach Hoy <hoyzach@gmail.com>
2023-09-12 17:13:10 -0400

    reorg imports

--- app/owned/page.jsx
+++ app/owned/page.jsx
@@ -2,11 +2,11 @@
 import { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHand, faArrowsRotate } from '@fortawesome/free-solid-svg-icons';
+import { ethers } from 'ethers';
 import { tokenData } from './tokenData';
+import { useContractReadLoop } from '../../utils/contractReads';
 import contractWrite from '../../utils/contractWrite';
 import useClientCheck from '../../utils/clientCheck';
-import { useContractReadLoop } from '../../utils/contractReads';
-import { ethers } from 'ethers';
 
 export default function Owned() {
   const [data, setData] = useState([]);
